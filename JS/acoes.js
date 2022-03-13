1b8611443cdbad4833489f11c3cbc524740877f1
José Carlos <etijcvivi@gmail.com>
2022-03-13 16:07:54 -0300

    Projeto Interactive Price - atualização acoes/js

--- JS/acoes.js
+++ JS/acoes.js
@@ -1,11 +1,38 @@
 var slider = document.querySelector("#campo");
 var saida = document.querySelector("#valor");
-console.log(slider.value);
+var precovenda = document.querySelector('#preco');
 
 saida.innerHTML = slider.value;
+precovenda.innerHTML = precovenda.value;
 
 slider.oninput = function() {
     saida.innerHTML = this.value;
+    var n = saida.innerHTML;
+     
+        if ((n >= 0) && (n < 8)) {
+            precovenda.value = '0.00';
+            precovenda.innerHTML = precovenda.value;
+        }
+        else if ((n >= 8) && (n < 50)) {
+            precovenda.value = '8.00';
+            precovenda.innerHTML = precovenda.value;
+        }
+        else if ((n >= 50) && (n < 100)) {
+            precovenda.value = '12.00';
+            precovenda.innerHTML = precovenda.value;
+        }
+        else if ((n >= '100') && (n < '500')) {
+            precovenda.value = '16.00';
+            precovenda.innerHTML = precovenda.value;
+        } 
+        else if ((n >= '500') && (n < '1000')) {
+            precovenda.value = '24.00';
+            precovenda.innerHTML = precovenda.value;
+        }
+        else {
+            precovenda.value = '36.00';
+            precovenda.innerHTML = precovenda.value;
+        }
 }
 
 slider.addEventListener("mousemove", function(){
