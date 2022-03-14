77bdee781283ccf0b0c07d8f4a78424205e65169
José Carlos <etijcvivi@gmail.com>
2022-03-13 23:24:51 -0300

    Projeto Interactive Price - atualização css.

--- JS/acoes.js
+++ JS/acoes.js
@@ -1,16 +1,15 @@
 var slider = document.querySelector("#campo");
 var saida = document.querySelector("#valor");
 var precovenda = document.querySelector('#preco');
-var desconto = document.querySelector('#toggle');
-console.log(desconto.checked);
+var mesano = document.querySelector('#toggle');
 
 saida.innerHTML = slider.value;
 
 slider.oninput = function() {
     saida.innerHTML = this.value;
     var n = saida.innerHTML;
-    var total_desc = precovenda.value;
-    if (desconto.checked = 'false') {
+
+    if (mesano.checked = true) {
         if ((n >= 0) && (n < 8)) {
             precovenda.value = '0.00';
             precovenda.innerHTML = precovenda.value;
@@ -34,39 +33,6 @@ slider.oninput = function() {
         else {
             precovenda.value = '36.00';
             precovenda.innerHTML = precovenda.value;
-        }
-    }
-    else {
-        if ((n >= 0) && (n < 8)) {
-            precovenda.value = 0;
-            total_desc.value = (precovenda.value * 0.25);
-            precovenda.innerHTML = total_desc.value;
-        }
-        else if ((n >= 8) && (n < 50)) {
-            precovenda.value = 8;
-            total_desc.value = (precovenda.value * 0.25);
-            console.log(tota_desc.value);
-            precovenda.innerHTML = total_desc;
-        }
-        else if ((n >= 50) && (n < 100)) {
-            precovenda.value = '12.00';
-            total_desc = (precovenda.value * 0.25);
-            precovenda.innerHTML = total_desc;
-        }
-        else if ((n >= 100) && (n < 500)) {
-            precovenda.value = '16.00';
-            total_desc = (precovenda.value * 0.25);
-            precovenda.innerHTML = total_desc;
-        } 
-        else if ((n >= 500) && (n < 1000)) {
-            precovenda.value = '24.00';
-            total_desc = (precovenda.value * 0.25);
-            precovenda.innerHTML = total_desc;
-        }
-        else {
-            precovenda.value = '36.00';
-            total_desc = (precovenda.value * 0.25);
-            precovenda.innerHTML = total_desc;
         }
     }
 }
